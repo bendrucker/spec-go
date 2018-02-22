631c3066a52c89c74ccd8163cb1bffbb7227fe64
Ben Drucker <bvdrucker@gmail.com>
2018-02-21 16:00:36 -0800

    Add variadic test

--- src/math.go
+++ src/math.go
@@ -4,6 +4,16 @@ func Add(a, b int) int {
   return a + b
 }
 
+func AddVariadic(addends... int) int {
+  sum := 0
+
+  for _, addend := range addends {
+    sum += addend
+  }
+
+  return sum
+}
+
 func Subtract(a, b int) int {
   return a - b
 }
--- src/math_test.go
+++ src/math_test.go
@@ -9,6 +9,10 @@ func TestAdd(t *testing.T) {
   assert.Equal(t, Add(1, 2), 3, "1 + 2 = 3")
 }
 
+func TestAddVariadic(t *testing.T) {
+  assert.Equal(t, AddVariadic(1, 2, 3, 4), 10, "1 + 2 + 3 + 4 = 10")
+}
+
 func TestSubtract(t *testing.T) {
   assert.Equal(t, Subtract(3, 1), 2, "3 - 1 = 2")
 }
