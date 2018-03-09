b02cf8c47598edf2b42be8f6d89df719946dcfb7
Ben Drucker <bvdrucker@gmail.com>
2018-03-08 17:52:29 -0800

    Add a test for sync module (once)

---
+++ src/sync.go
@@ -0,0 +1,17 @@
+package spec
+
+import "sync"
+
+func OnceIncrement(value int) int {
+  var once sync.Once
+
+  once.Do(func() {
+    value++
+  })
+
+  once.Do(func() {
+    value++
+  })
+
+  return value
+}
---
+++ src/sync_test.go
@@ -0,0 +1,10 @@
+package spec
+
+import (
+  "testing"
+  "github.com/stretchr/testify/assert"
+)
+
+func TestOnceIncrement(t *testing.T) {
+  assert.Equal(t, OnceIncrement(0), 1)
+}
