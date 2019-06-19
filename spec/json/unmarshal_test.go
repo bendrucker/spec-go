29c3610a27e0622a81ad9ba70e4981f008826c5e
Ben Drucker <bvdrucker@gmail.com>
2019-06-19 08:08:24 -0700

    add unmarshaling json to struct

--- spec/json/unmarshal_test.go
+++ spec/json/unmarshal_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestUnmarshalKvJson(t *testing.T) {
+func TestUnmarshalMapInterface(t *testing.T) {
 	encoded := []byte(`
 		{"message": "Hello, world!", "i": 1}
 	`)
@@ -17,4 +17,20 @@ func TestUnmarshalKvJson(t *testing.T) {
 
 	assert.Equal(t, "Hello, world!", decoded["message"].(string))
 	assert.Equal(t, 1, int(decoded["i"].(float64)))
+}
+
+func TestUnmarshallStruct(t *testing.T) {
+	type event struct {
+		Message string `json:"message"`
+		Index   int    `json:"i"`
+	}
+	encoded := []byte(`
+		{"message": "Hello, world!", "i": 1}
+	`)
+
+	decoded := event{}
+	json.Unmarshal(encoded, &decoded)
+
+	assert.Equal(t, "Hello, world!", decoded.Message)
+	assert.Equal(t, 1, decoded.Index)
 }
