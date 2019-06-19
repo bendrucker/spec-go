930d3c15e2c0549caf7adcef9e1ce0b2254d7a45
Ben Drucker <bvdrucker@gmail.com>
2019-06-19 07:58:43 -0700

    fix spelling of unmarhsal

--- spec/json/unmarshal_test.go
+++ spec/json/unmarshal_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestUnmarshallKvJson(t *testing.T) {
+func TestUnmarshalKvJson(t *testing.T) {
 	encoded := []byte(`
 		{"message": "Hello, world!", "i": 1}
 	`)
