4c79bca5ede938f11ad3c508969ff1757c113b5c
Ben Drucker <bvdrucker@gmail.com>
2019-06-24 19:41:26 -0700

    add http example with Client, test Server, and Mux

---
+++ spec/http/http_client_test.go
@@ -0,0 +1,63 @@
+package http
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func setup() (*http.ServeMux, *httptest.Server, func()) {
+	mux := http.NewServeMux()
+	server := httptest.NewServer(mux)
+
+	return mux, server, func() {
+		server.Close()
+	}
+}
+
+func TestHttpHead(t *testing.T) {
+	resp, err := http.Head("https://golang.org")
+
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	assert.Equal(t, 200, resp.StatusCode)
+}
+
+func TestHttptestMux(t *testing.T) {
+	mux, server, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodHead, r.Method)
+	})
+
+	resp, err := http.Head(server.URL)
+
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	assert.Equal(t, 200, resp.StatusCode)
+}
+
+func TestHttptestStatus(t *testing.T) {
+	mux, server, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodHead, r.Method)
+		w.WriteHeader(404)
+	})
+
+	resp, err := http.Head(server.URL)
+
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	assert.Equal(t, 404, resp.StatusCode)
+}
