9a814269e61e721e3c53606bf3a1697ced97c4bc
Ben Drucker <bvdrucker@gmail.com>
2019-07-06 18:37:07 -0700

    consolidate sync.Once test

--- spec/sync/sync_test.go
+++ spec/sync/sync_test.go
@@ -1,11 +1,26 @@
 package sync
 
 import (
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestOnceIncrement(t *testing.T) {
-	assert.Equal(t, OnceIncrement(0), 1)
+	onceIncrement := func(value int) int {
+		var once sync.Once
+
+		once.Do(func() {
+			value++
+		})
+
+		once.Do(func() {
+			value++
+		})
+
+		return value
+	}
+
+	assert.Equal(t, onceIncrement(0), 1)
 }
